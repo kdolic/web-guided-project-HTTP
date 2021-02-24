277da29687680b661b610372c0dc4413aed1b870
Kenan Dolic <kdolic@alumni.iastate.edu>
2021-02-24 13:06:13 -0600

    lecture project

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Route, NavLink } from 'react-router-dom';
+import { Route, NavLink, useHistory, useParams } from 'react-router-dom';
 import axios from 'axios';
 
 import ItemDescription from './ItemDescription';
@@ -7,7 +7,8 @@ import ItemShipping from './ItemShipping';
 
 function Item(props) {
   const [item, setItem] = useState({});
-  const { id } = props.match.params;
+  const { id } = useParams();
+  const {push} = useHistory();
 
   console.log();
   useEffect(()=>{
@@ -19,6 +20,20 @@ function Item(props) {
 
   if (!item) {
     return <h2>Loading item data...</h2>;
+  }
+
+  const handleEditClick = () => {
+    props.history.push(`/item-update/${id}`)
+  }
+
+  const handleDeleteClick = () => {
+    axios
+    .delete(`http://localhost:3333/items/${id}`)
+    .then(res => {
+      props.setItems(res.data)
+      push('/item-list')
+    })
+    .catch(err => console.log(err))
   }
 
   return (
@@ -47,10 +62,10 @@ function Item(props) {
         path="/item-list/:id/shipping"
         render={props => <ItemShipping {...props} item={item} />}
       />
-      <button className="md-button">
+      <button onClick={handleEditClick} className="md-button">
         Edit
       </button>
-      <button className="md-button">
+      <button onClick={handleDeleteClick} className="md-button">
         Delete
       </button>
     </div>
--- client/src/index.js
+++ client/src/index.js
@@ -51,6 +51,7 @@ const App = () => {
         render={props => <Item {...props} setItems={setItems} />}
       />
       <Route path="/item-form" component={ItemForm} />
+      <Route path='/item-update/:id' render= {props => <UpdateForm {...props} setItems={setItems} />} />
     </div>
   );
 };
